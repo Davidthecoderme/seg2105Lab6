8b1c4d06731d33739444832546d76090288a00a4
D-codeTillDie <125109506+Davidthecoderme@users.noreply.github.com>
2023-11-06 13:56:43 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "seg2105Lab6"
+include(":app")
+ 
